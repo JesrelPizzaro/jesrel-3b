70a1e1808b8a4edd060fced4cf717a0603c16d7b
JESREL PENTECOSTES PIZZARO <145620113+JesrelPizzaro@users.noreply.github.com>
2024-02-25 19:19:43 +0800

    Update 0_xor_cipher.py

--- pages/0_xor_cipher.py
+++ pages/0_xor_cipher.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header("Welcome to XOR Cipher!")
-st.writer("What is your name")
+st.write("What is your name")
 
 txt_FNAME = st.text_input("FIRST NAME")
 txt_LNAME = st.text_input("LAST NAME")
